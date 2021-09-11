8c0b0539a9ffdc56d49b427d6b05e847eaa3bb08
Katie <kitkatie117@gmail.com>
2021-09-11 14:54:09 -0400

    Initial Commit

---
+++ Dice Roll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice Roll
+//
+//  Created by Katie Xu on 9/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
